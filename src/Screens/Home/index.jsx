d8206ff59d57c2e8f8e3a5c1333209914bfdeec5
efrainmpp1 <efrainmpp@hotmail.com>
2025-03-07 18:20:09 -0300

    add link button to exercises on Home Page

--- src/Screens/Home/index.jsx
+++ src/Screens/Home/index.jsx
@@ -1,14 +1,19 @@
 import "./index.css";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <>
       <h1>Teste Prático</h1>
       <div className="exercises_list">
-        <button>Exercício 1 - Caixa Eletrônico</button>
-        <button>
-          Exercício 2 - Identificar a(s) letra(s) mais Frequente(s)
-        </button>
+        <Link to="/exercise1">
+          <button>Exercício 1 - Caixa Eletrônico</button>
+        </Link>
+        <Link to="/exercise2">
+          <button>
+            Exercício 2 - Identificar a(s) letra(s) mais Frequente(s)
+          </button>
+        </Link>
       </div>
     </>
   );
