e0fa2ef937a133826ffe9ea2f40d07f1c2d96a75
efrainmpp1 <efrainmpp@hotmail.com>
2025-03-07 18:15:03 -0300

    Insert new base Screens

---
+++ src/Screens/Exercise1/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Exercise1 = () => {
+  return (
+    <div>
+      <h1>Exercise 1</h1>
+      <p>This is a simple React component.</p>
+    </div>
+  );
+};
+
+export default Exercise1;
---
+++ src/Screens/Home/index.jsx
@@ -0,0 +1,17 @@
+import "./index.css";
+
+function Home() {
+  return (
+    <>
+      <h1>Teste Prático</h1>
+      <div className="exercises_list">
+        <button>Exercício 1 - Caixa Eletrônico</button>
+        <button>
+          Exercício 2 - Identificar a(s) letra(s) mais Frequente(s)
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Home;
